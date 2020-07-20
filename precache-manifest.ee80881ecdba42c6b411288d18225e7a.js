c16f89fcbc638d569be783caf73f6c9d2cdb23ef
StackiCode <stacki.code@gmail.com>
2020-07-20 15:04:43 +0700

    Updates

---
+++ precache-manifest.ee80881ecdba42c6b411288d18225e7a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f108acbe6f1e76f42eba3fbe9dcd3839",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4da09fe405b910836105",
+    "url": "/portfolio/static/css/main.f58a1adc.chunk.css"
+  },
+  {
+    "revision": "612e7ccb15d15e401eb5",
+    "url": "/portfolio/static/js/2.c3a3cd11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.c3a3cd11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4da09fe405b910836105",
+    "url": "/portfolio/static/js/main.60183f9b.chunk.js"
+  },
+  {
+    "revision": "b6e55f568c76f167d285",
+    "url": "/portfolio/static/js/runtime-main.70d117b2.js"
+  },
+  {
+    "revision": "b26b20369adfef2b3d65d266e0625fe2",
+    "url": "/portfolio/static/media/Gabriola.b26b2036.ttf"
+  },
+  {
+    "revision": "1c844dba3abfde20f7003e8eb999e4b1",
+    "url": "/portfolio/static/media/P1.1c844dba.png"
+  },
+  {
+    "revision": "61d6f343d64391d6df1f674074962b16",
+    "url": "/portfolio/static/media/P2.61d6f343.png"
+  },
+  {
+    "revision": "5d3efe76f6cee53115f81164b18e2f54",
+    "url": "/portfolio/static/media/my-photo.5d3efe76.png"
+  }
+]);
